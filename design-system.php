0211e766859855c698ca8e8d230643fe77ed48a4
Ruben <contact@rubenmadila.com>
2024-03-19 22:58:10 +0000

    Update design-system.php

--- design-system.php
+++ design-system.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Design System
  * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.4
+ * Requires at least: 6.5
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            Ruben Madila
